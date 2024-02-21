06dfaf1a60d0ac158a4da2f22fb012d7a4d8ada3
ViniciusBelasco <vbrafaeli@gmail.com>
2024-02-20 23:25:46 -0300

    Add loops and concatenation sheet for practice

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-function Main()
+local function Main()
 	local sum = 0
 	local aux
 
@@ -10,8 +10,25 @@ function Main()
 	for i = 1, 5 do
 		aux = math.random(6)
 		io.write(aux)
-		sum = sum * aux
+		sum = sum + aux
 	end
 
 	io.write("\n")
+	io.write("The sum of the dice is " .. sum .. "\n")
+
+	io.write("Lightning Bolt from a level 10 wizard dealt ", LightningBolt(10), " damage. \n")
+	io.write("Lightning Bolt from a level 20 wizard dealt ", LightningBolt(20), " damage. \n")
 end
+
+function LightningBolt(level)
+	local damage = 0
+
+	for i = 1, level do
+		local aux = math.random(6)
+		damage = damage + aux
+	end
+
+	return damage
+end
+
+Main()
