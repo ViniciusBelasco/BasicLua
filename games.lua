e3bf32977a722946fd03660d11c26a5a942a662d
ViniciusBelasco <vbrafaeli@gmail.com>
2024-02-22 22:36:56 -0300

    Finish basic guess game

--- games.lua
+++ games.lua
@@ -1,16 +1,41 @@
 local function Main()
-	math.randomseed(os.time())
-	math.random()
+	local whichgame = 0
 
-	local number = math.random(100)
-	local count = 0
-	local guess
+	io.write("Pick a game \n")
+	io.write("1) Guess game ")
+	io.write("")
+	io.write("")
 
-	io.write("Guess my number (1=100): ")
-	guess = io.read("*n")
-  count = count + 1
+	whichgame = io.read("*n")
 
+	if whichgame == 1 then
+		--guess the number
+		math.randomseed(os.time())
+		math.random()
 
+		local number = math.random(100)
+		local count = 0
+		local guess
+
+		io.write("Guess my number (1=100): ")
+		guess = io.read("*n")
+		count = count + 1
+
+		while guess ~= number do
+			if guess > number then
+				io.write(guess, " is too high.")
+			elseif guess < number then
+				io.write(guess, "is too low.")
+			end
+
+			io.write("Guess again: ")
+			guess = io.read("*n")
+			count = count + 1
+		end
+	elseif whichgame == 2 then
+    --improved guess
+	elseif whichgame == 3 then
+	end
 end
 
 Main()
