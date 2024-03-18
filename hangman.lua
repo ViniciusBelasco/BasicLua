8e01f352a29a0e6b57db7f45dd7bb07f03832b36
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-17 22:45:48 -0300

    Remove game loop

--- hangman.lua
+++ hangman.lua
@@ -1,147 +1,135 @@
 function main()
-	while true do
-		local phrase, lives, aux
-		local ending = false
-		local auxPhrase
-		local said = ""
+	local phrase, lives, aux
+	local ending = false
+	local auxPhrase
+	local said = ""
 
-		io.write("enter a word or phrase to begin (this game is not key sensitive): ")
-		phrase = io.read("*l")
-		auxPhrase = phrase:upper()
-		io.write("Enter maximum lives: ")
-		lives = io.read("*n")
+	io.write("enter a word or phrase to begin (this game is not key sensitive): ")
+	phrase = io.read("*l")
+	auxPhrase = phrase:upper()
+	io.write("Enter maximum lives: ")
+	lives = io.read("*n")
 
-		if lives == nil then
-			io.write("Please type the number of lifes for this game.")
-			goto continue
+	aux = phrase:len()
+
+	for k = 1, phrase:len() do
+		local x = phrase:sub(k, k)
+		if x == " " or x == "'" then
+			aux = aux - 1
+		end
+	end
+
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+	io.write("\n")
+
+	io.write("This is hangman game. You have to figure out the word by typing letters.\n")
+	io.write("You have a maximum of ", lives, " lives to solve the word and winning the game.\n")
+	io.write("Let's get started!!\n\n")
+	io.write("the word has ", aux, " letters.\n\n")
+
+	local letter
+
+	local build = { phrase:len() }
+
+	for i = 1, phrase:len() do
+		local x = phrase:sub(i, i)
+
+		if x == " " then
+			build[i] = " "
+		elseif x == "'" then
+			build[i] = "'"
+		else
+			build[i] = "_"
+		end
+	end
+
+	for i = 1, phrase:len() do
+		io.write(build[i])
+		if i == phrase:len() then
+			io.write("\n")
+			io.write("\n")
+		end
+	end
+
+		io.write("\n")
+
+		if aux == 0 then
+			ending = true
 		end
 
-		aux = phrase:len()
+		while true do
+			while letter:len() ~= 1 or letter == " " do -- this control single letter input
+				io.write("Please enter just 1 letter and no white spaces: ")
+				letter = io.read("*l")
+				letter = letter:upper()
+			end
 
-		for k = 1, phrase:len() do
-			local x = phrase:sub(k, k)
-			if x == " " or x == "'" then
-				aux = aux - 1
+			if letter:len() == 1 then
+				break
 			end
 		end
 
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-		io.write("\n")
-
-		io.write("This is hangman game. You have to figure out the word by typing letters.\n")
-		io.write("You have a maximum of ", lives, " lives to solve the word and winning the game.\n")
-		io.write("Let's get started!!\n\n")
-		io.write("the word has ", aux, " letters.\n\n")
-
-		local letter
-
-		local build = { phrase:len() }
+		local j = 0
 
 		for i = 1, phrase:len() do
-			local x = phrase:sub(i, i)
+			local c = auxPhrase:sub(i, i)
 
-			if x == " " then
-				build[i] = " "
-			elseif x == "'" then
-				build[i] = "'"
-			else
-				build[i] = "_"
+			if c == letter then
+				said = said .. letter
+				build[i] = letter
+				aux = aux - 1
+				j = j + 1
 			end
 		end
 
 		for i = 1, phrase:len() do
 			io.write(build[i])
-			if i == phrase:len() then
-				io.write("\n")
-				io.write("\n")
-			end
 		end
 
-		while not ending do
-			io.write("Enter a letter, please: ")
-			letter = io.read("*l")
-			letter = letter:upper()
-			io.write("\n")
+		io.write("\n")
+		io.write("\n\n")
 
-			if aux == 0 then
-				ending = true
-			end
+		if j == 0 then
+			lives = lives - 1
+			io.write("There is no ", letter, " in the word. You lost one live!\n")
+			io.write(lives, " live(s) left\n\n")
 
-			while true do
-				while letter:len() ~= 1 or letter == " " do -- this control single letter input
-					io.write("Please enter just 1 letter and no white spaces: ")
-					letter = io.read("*l")
-					letter = letter:upper()
-				end
-
-				if letter:len() == 1 then
-					break
-				end
-			end
-
-			local j = 0
-
-			for i = 1, phrase:len() do
-				local c = auxPhrase:sub(i, i)
-
-				if c == letter then
-					said = said .. letter
-					build[i] = letter
-					aux = aux - 1
-					j = j + 1
-				end
-			end
-
-			for i = 1, phrase:len() do
-				io.write(build[i])
-			end
-
-			io.write("\n")
-			io.write("\n\n")
-
-			if j == 0 then
-				lives = lives - 1
-				io.write("There is no ", letter, " in the word. You lost one live!\n")
-				io.write(lives, " live(s) left\n\n")
-
-				if lives == 0 then
-					ending = true
-				end
-			end
-
-			if aux > 0 then
-				io.write("There are ", aux, " Letters left\n\n")
-			else
+			if lives == 0 then
 				ending = true
 			end
 		end
 
-		if lives == 0 then
-			io.write("SORRY!! You lost the game!\n")
-		elseif aux == 0 and lives > 0 then
-			io.write("YAI!! CONGRATULATIONS!! YOU WON! \n")
+		if aux > 0 then
+			io.write("There are ", aux, " Letters left\n\n")
+		else
+			ending = true
 		end
-		io.write("The word was ", auxPhrase, "\n")
-		::continue::
 	end
+
+	if lives == 0 then
+		io.write("SORRY!! You lost the game!\n")
+	elseif aux == 0 and lives > 0 then
+		io.write("YAI!! CONGRATULATIONS!! YOU WON! \n")
+	end
+	io.write("The word was ", auxPhrase, "\n")
 end
 
 main()
